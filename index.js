f0381dcd2ecd9217850dc83317129e18b2fbe40f
Carlos <carlos.alfonso.montero@gmail.com>
2021-03-19 16:56:29 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+// Removing main
+//const main = document.querySelector("#main");
+const main = document.getElementById("main");
+main.remove()
+
+// Adding h1
+const newHeader = document.createElement('h1');
+
+// Assign id of victory
+newHeader.id = "victory";
+
+// Adding content to header
+newHeader.innerHTML = "Carlos is the champion"; 
+document.body.appendChild(newHeader);
